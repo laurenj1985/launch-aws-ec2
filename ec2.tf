5a62cb20cacf8f898fed1773b14fe6930b8e4586
Lauren Johnson <lchristopher.johnson@gmail.com>
2023-09-13 11:39:29 -0400

    update ec2 tf file

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,17 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "terraform-user"
+  profile = "CloudReachLauren"
 }
 
 
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
+    bucket  = "cicd-tf-test-laurenj-state"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
-    profile = "terraform-user"
+    profile = "CloudReachLauren"
   }
 }
 
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "project2023"
   user_data              = file("install_techmax.sh")
 
   tags = {
